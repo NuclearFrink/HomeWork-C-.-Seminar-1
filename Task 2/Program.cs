090c26a8bb72992c4e202c9b5bbd61a6a33e9413
Grigoriy <kutsaevg@yandex.ru>
2023-03-13 19:59:18 +0300

    Выполнение домашнего задания к первому семинару

---
+++ Task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число: ");
+int num_X = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num_Y = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int num_Z = int.Parse(Console.ReadLine());
+
+int max = num_X; 
+if (num_Y > max)
+{
+    max = num_Y;
+}
+if (num_Z > max)
+{
+    max = num_Z;
+}
+Console.WriteLine("Наибольшее из введённых чисел -> " + max);
